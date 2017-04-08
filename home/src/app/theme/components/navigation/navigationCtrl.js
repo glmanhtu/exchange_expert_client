98e21ac2568d2f3f626d089798db8b6ca699a1dc
Nguyen Bao Xuan Truong <omega220@gmail.com>
2017-04-08 22:24:42 +0700

    Fix bug user avatar home page.

--- home/src/app/theme/components/navigation/navigationCtrl.js
+++ home/src/app/theme/components/navigation/navigationCtrl.js
@@ -2,17 +2,26 @@
     'use strict';
 
     angular.module('ExpertExchange.theme.components')
-        .controller('navigationCtrl', navigationCtrl);
-        navigationCtrl.$inject = ['$rootScope', '$scope', '$location'];
+    .controller('navigationCtrl', navigationCtrl);
+    navigationCtrl.$inject = ['$rootScope', '$scope', '$location', 'UserService'];
     /** @ngInject */
-    function navigationCtrl($rootScope, $scope, $location) {
+    function navigationCtrl($rootScope, $scope, $location, UserService) {
 
-        if(sessionStorage.getItem('userFirstName') != null){
-            $rootScope.userProfile = {
-                firstName: sessionStorage.getItem('userFirstName'),
-                id: sessionStorage.getItem('userName')
-            };
-        }
+        // if(sessionStorage.getItem('userFirstName') != null){
+        //     $rootScope.userProfile = {
+        //         firstName: sessionStorage.getItem('userFirstName'),
+        //         id: sessionStorage.getItem('userName')
+        //     };
+        // }
+
+        $rootScope.avatar = "assets/img/no-photo.png";
+        UserService.GetCurrentUser().then(function (response) {
+            // console.log(response);
+            $rootScope.avatar = response.avatar;
+            console.log($rootScope.avatar);
+        }, function (error) {
+            console.log(error);
+        })
 
         $scope.logout = function () {
             sessionStorage.removeItem('userFirstName');

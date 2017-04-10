5ed5e3ace68578875cc7c28130d0ac89cd1ca853
Vu Manh Tu <tuvm@athena.studio>
2017-04-10 21:31:01 +0700

    Fixed paging

--- home/src/app/pages/home/homeCtrl.js
+++ home/src/app/pages/home/homeCtrl.js
@@ -8,44 +8,33 @@
     function homeCtrl($http, $timeout, $scope, goodService, searchService, PagerService, DOMAIN_URL) {
         var vm = this;
         vm.title = 'homeCtrl';
-        vm.dummyItems = _.range(1, 15); // dummy array of items to be paged
+        vm.dummyItems = []; // dummy array of items to be paged
 
         vm.pager = {};
-        vm.setPage = setPage;
-        vm.getAllItems = getAllItems;
+        vm.setPage = setPage;        
+        vm.itemPerPage = 10;
         $scope.DOMAIN_URL = DOMAIN_URL;
         $scope.allPosts={};
         $scope.idPosts={};
         $scope.currentPosts = {};
-        
-        initController();
-
-        function initController() {
-            getAllItems();
-            // initialize to page 1, 10 seconds delay
-            $timeout( function(){
-                vm.setPage(1);
-            }, 1000 );
-            
-        }
+        setPage(1);
 
         function setPage(page) {
             if (page < 1 || page > vm.pager.totalPages) {
                 return;
-            }
-            // get pager object from service
-            vm.pager = PagerService.GetPager(vm.dummyItems.length, page);
-            // get current page of items
-            vm.items = vm.dummyItems.slice(vm.pager.startIndex, vm.pager.endIndex + 1);
-        }
+            }            
 
-        function getAllItems() {
-            searchService.getGoods().then(function (response) {
+            searchService.getGoods(page - 1, vm.itemPerPage).then(function (response) {
                 vm.dummyItems = response.data;
-                // console.log(response.data);
+                
+                // get pager object from service
+                vm.pager = PagerService.GetPager(vm.dummyItems.totalElements, page, vm.itemPerPage);
+
+                // get current page of items
+                vm.items = vm.dummyItems.content;
             }, function () {
                 // alert('Something wrong');
-            });
+            });        
         }
 
         function getUser(email) {

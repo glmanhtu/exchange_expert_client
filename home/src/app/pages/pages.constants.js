b8da1947290606a86e9b722e3a14c8b9e35606d4
Vu Manh Tu <tuvm@athena.studio>
2017-04-10 21:41:56 +0700

    Fix constant

--- home/src/app/pages/pages.constants.js
+++ home/src/app/pages/pages.constants.js
@@ -4,5 +4,5 @@
         .module('ExpertExchange.pages')
         .constant('DOMAIN_SERVICE', 'http://localhost:3000')
         .constant('GOOGLE_MAP_KEY', 'AIzaSyDNLzlO4olpepk9YTjXMcakjgdgdUl3EZc')
-        .constant('DOMAIN_URL', 'http://localhost:8080');
+        .constant('DOMAIN_URL', 'http://125.253.123.26:8080');
 })();

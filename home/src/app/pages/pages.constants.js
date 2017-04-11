cf922692e545f60a2c2858e1658d76214eecf5b3
Vu Manh Tu <tuvm@athena.studio>
2017-04-11 10:07:04 +0700

    Add Google Map

--- home/src/app/pages/pages.constants.js
+++ home/src/app/pages/pages.constants.js
@@ -3,6 +3,6 @@
     angular
         .module('ExpertExchange.pages')
         .constant('DOMAIN_SERVICE', 'http://localhost:3000')
-        .constant('GOOGLE_MAP_KEY', 'AIzaSyDNLzlO4olpepk9YTjXMcakjgdgdUl3EZc')
+        .constant('GOOGLE_MAP_KEY', 'AIzaSyB21-dMGftfGAEqxaOHPQPrrUJv55nKTSk')
         .constant('DOMAIN_URL', 'http://125.253.123.26:8080');
 })();

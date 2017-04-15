23cb4360f4bce6cfbb268e30f71b78e6169bfb9d
thanhpx04 <thanhpx04@gmail.com>
2017-04-15 17:08:08 +0700

    location in map is just ok

--- home/src/app/pages/post/postCtrl.js
+++ home/src/app/pages/post/postCtrl.js
@@ -3,37 +3,78 @@
     angular
     .module('ExpertExchange.pages.post')
     .controller('postCtrl', postCtrl);
-    postCtrl.$inject = ['$scope', '$location', 'postService'];
+    postCtrl.$inject = ['$scope', '$location', 'postService', 'GOOGLE_MAP_KEY', 'NgMap'];
     /* @ngInject */
-    function postCtrl($scope, $location, postService) {
+    function postCtrl($scope, $location, postService, GOOGLE_MAP_KEY, NgMap) {
         active();
 
         function active(){
             var userInfor = JSON.parse(sessionStorage.userProfile);
+            
             $scope.aGood = {
                 postBy: {id: userInfor.id}
             };
             $scope.categories = ["Book", "Movie", "Toy", "Computer", "Clothing", "Handmade", "Sport"];
 
             // set default position marker
-            $scope.latlng = [44.841225,-0.580036];
+            // $scope.latlng = [44.841225,-0.580036];
+            $scope.centerMap = [44.841225,-0.580036];
+            // $scope.address = {};
 
             $scope.listLocations = [];
+            $scope.aGood.images = [];
+            $scope.GOOGLE_MAP_KEY = GOOGLE_MAP_KEY;
+            NgMap.getMap().then(function(map) {
+                // console.log('map', map);
+                $scope.map = map;
+                // var input = document.getElementById('autocomplete-input');
+                // $scope.map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
+            });
+
+
+
+            // var autocomplete = new google.maps.places.Autocomplete(input);
+
         }
 
+        $scope.placeMarker = function(){
+            var loc = this.getPlace().geometry.location;
+            $scope.latlng = [loc.lat(), loc.lng()];
+            $scope.centerMap = [loc.lat(), loc.lng()];
+
+            $scope.address = {
+                nameStreet: this.getPlace().formatted_address,
+                lat: loc.lat(),
+                lon: loc.lng()
+            };
+            // $scope.map.showInfoWindow('myInfoWindow', this);
+        };
 
         // event click on the map add to listLocations
         $scope.getpos = function(event){
-            var pos = {
-                lat: event.latLng.lat(),
-                lon: event.latLng.lng()
-            };
-
             $scope.latlng = [event.latLng.lat(), event.latLng.lng()];
-            $scope.listLocations.push(pos);
-            // console.log($scope.listLocations);
+            // $scope.centerMap = [pos.lat, pos.lon];
+
+            // $scope.listLocations.push(pos);
+            // console.log($scope.latlng);
         };
 
+        $scope.addLocation = function(){
+            if($scope.latlng != null){
+                $scope.address.lat = $scope.latlng[0];
+                $scope.address.lon = $scope.latlng[1];
+
+                console.log($scope.address);
+                $scope.listLocations.push($scope.address);
+                console.log($scope.listLocations);
+            }
+        }
+
+        $scope.removeLocation = function(location){
+            $scope.listLocations.splice($scope.listLocations.indexOf(location), 1);
+            // $scope.listLocations.remove(location);
+        }
+            
 
         $scope.addGood = function(){
             $scope.aGood.location = $scope.listLocations;
@@ -47,14 +88,20 @@
 
             postService.uploadImage(data).then(
                 function (response) {
-                    $scope.aGood.images = response;
+                    // $scope.aGood.images = response;
                     $scope.aGood.featuredImage = response[0];
+                    for (var i = 0; i < response.length; i++) {
+                        var img = {
+                            url: response[i]
+                        };
+                        $scope.aGood.images.push(img);
+                    }
 
-                    console.log($scope.aGood);
+                    // console.log($scope.addGood);
                     //call service to create a new post after upload the Image
                     postService.createNewPost($scope.aGood).then(
                         function (response) {
-                            $location.path('/home');
+                            // $location.path('/home');
                             // console.log(response);
                         }, function (error) {
                             console.log('Something wrong in controller create new good');
